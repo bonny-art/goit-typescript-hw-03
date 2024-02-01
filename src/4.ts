33d40813ed105d8b9ab477987bca8844f198b49c
bonny-art <svitlana.otenko@gmail.com>
2024-02-02 00:43:15 +0200

    adds solutions to all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,98 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * (999 - 1 + 1) + 1);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key, public name: string) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  public comeIn(person: Person): void {
+    console.log("An attempt to come in...");
+    if (this.door === false) {
+      console.log("The door is locked!");
+      return;
+    }
+    this.tenants.push(person);
+    this.door = false;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+
+  public abstract openDoor(obj: Key): void;
+}
+
+class MyHouse extends House {
+  public door: boolean = false;
+
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  public openDoor(key: Key): void {
+    console.log("An attempt to open the door...");
+    if (key !== this.key) {
+      console.log("You have invalid key!");
+      return;
+    }
+    this.door = true;
+  }
+}
+
 const key = new Key();
+console.log("🚀 ~ key:", key);
 
 const house = new MyHouse(key);
-const person = new Person(key);
+console.log("🚀 ~ house:", house);
+const houseKey = house.getKey();
+console.log("🚀 ~ houseKey:", houseKey);
 
-house.openDoor(person.getKey());
+const max = new Person(key, "Max");
+console.log("🚀 ~ person:", max);
 
-house.comeIn(person);
+const isOpenedBefore = house.door;
+console.log("🚀 ~ isOpenedBefore:", isOpenedBefore);
 
+house.openDoor(max.getKey());
+const isOpenedAfter = house.door;
+console.log("🚀 ~ isOpenedAfter:", isOpenedAfter);
+
+house.comeIn(max);
+
+console.log("🚀 ~ house:", house);
+
+const key2 = new Key();
+console.log("🚀 ~ key2:", key2);
+
+const bob = new Person(key2, "Bob");
+console.log("🚀 ~ person2:", bob);
+house.openDoor(bob.getKey());
+house.comeIn(bob);
+console.log("🚀 ~ house:", house);
+
+const mary = new Person(key, "Mary");
+console.log("🚀 ~ person2:", mary);
+house.openDoor(mary.getKey());
+house.comeIn(mary);
+console.log("🚀 ~ house:", house);
 
 export {};
